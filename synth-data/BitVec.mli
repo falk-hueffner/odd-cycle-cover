28ec2804c984a1535fda175ef628978d86beb5ca
Falk Hueffner <hueffner@minet.uni-jena.de>
2004-08-10 10:47:21 +0000

    * Fix missing Bitvec.of_list * Fix documentation

--- synth-data/BitVec.mli
+++ synth-data/BitVec.mli
@@ -1,56 +1,77 @@
 (** Module for handling (imperative) bit vectors.  *)
 
-(** The type of a bit vector.  *)
 type t
+(** The type of a bit vector.  *)
+
+val create : int -> t
 (** [create n] returns a new bit vector of length [n] with all bits
     set to 0.  *)
-val create : int -> t
+
+val create_filled : int -> t
 (** [create_filled n] returns a new bit vector of length [n] with all bits
     set to 1.  *)
-val create_filled : int -> t
-(** Returns a copy of a bit vector.  *)
+
 val copy : t -> t
-(** Returns the length of a bit vector.  *)
+(** Returns a copy of a bit vector.  *)
+
 val length : t -> int
+(** Returns the length of a bit vector.  *)
+
+val compare : t -> t -> int
 (** Behaves like [Pervasives.compare], for an unspecified total ordering
     on bit vectors.  *)
-val compare : t -> t -> int
-(** [is_set s n] returns [true] iff bit [n] is set in [s].  *)
+
 val is_set : t -> int -> bool
-(** [set s n] sets bit [n] in [s] to 1.  *)
+(** [is_set s n] returns [true] iff bit [n] is set in [s].  *)
+
 val set : t -> int -> unit
-(** [set s n] sets bit [n] in [s] to 0.  *)
+(** [set s n] sets bit [n] in [s] to 1.  *)
+
 val unset : t -> int -> unit
-(** [set s n v] sets bit [n] in [s] to [v].  *)
+(** [set s n] sets bit [n] in [s] to 0.  *)
+
 val put : t -> int -> bool -> unit
-(** [toggle s n v] behaves like [put s n (not (get s n))].  *)
+(** [set s n v] sets bit [n] in [s] to [v].  *)
+
 val toggle : t -> int -> unit
-(** [iter f s] calls [f n] for each [n] where [s] has a 1-bit .  *)  
-val iter : (int -> unit) -> t -> unit
+(** [toggle s n v] behaves like [put s n (not (get s n))].  *)
+
 val not_found : int
+val iter : (int -> unit) -> t -> unit
+(** [iter f s] calls [f n] for each [n] where [s] has a 1-bit .  *)  
+
+val find_0 : t -> int -> int
 (** [find_0 s n] returns the first position in [s] larger or equal
     than [n] that contains a 0-bit, or [not_found] if no such position
     exists.  *)
-val find_0 : t -> int -> int
-(** Returns the number of 1-bits in the set.  *)
+
 val count : t -> int
+(** Returns the number of 1-bits in the set.  *)
+
+val resize : t -> int -> t
 (** [resize s n] returns a copy of [s] of size [n], with bits cropped
     or padded with zero.  *)
-val resize : t -> int -> t
-(** Returns an array containing the posisions of the 1-bits.  *)
+
 val to_array : t -> int array
+(** Returns an array containing the posisions of the 1-bits.  *)
+
+val inverse : t -> t
 (** Returns a copy of the input bit vector with 1-bits replaced by
     0-bits and vice versa.  *)
-val inverse : t -> t
+
+val setminus : t -> t -> t
 (** [setminus s1 s2] returns a copy of [s1] with positions that have a
     1-bit in [s2] cleared.  *)
-val setminus : t -> t -> t
+
+val union : t -> t -> t
 (** [union s1 s2] returns a copy of the larger of [s1] and [s2] with
     positions that have a 1-bit in the other set to 1.  *)
-val union : t -> t -> t
-(** [output c s] prints a debug representation of [s] to channel [c].  *)
+
 val output : out_channel -> t -> unit
-(** [print s] is the same as [output stdout s].  *)
+(** [output c s] prints a debug representation of [s] to channel [c].  *)
+
 val print : t -> unit
-(** [dump s] is the same as [output stderr s].  *)
+(** [print s] is the same as [output stdout s].  *)
+
 val dump : t -> unit
+(** [dump s] is the same as [output stderr s].  *)
--- synth-data/Graph.mli
+++ synth-data/Graph.mli
@@ -2,29 +2,39 @@
     identified with integers. Modifying is very expensive, the data
     structure is targeted towards mainly being queried.  *)
       
-(** The type of a graph.  *)
 type t
-(** Returns a new graph.  *)
+(** The type of a graph.  *)
+
 val create : unit -> t
-(** Returns the highest number of a vertex that was connected, plus one.  *)
+(** Returns a new graph.  *)
+
 val size : t -> int
-(** Returns a copy of a graph.  *)
+(** Returns the highest number of a vertex that was connected, plus one.  *)
+
 val copy : t -> t
-(** [connect g v w] connects vertices [v] and [w] in [g]. O(n^2)  *)
+(** Returns a copy of a graph.  *)
+
 val connect : t -> int -> int -> unit
+(** [connect g v w] connects vertices [v] and [w] in [g]. O(n^2)  *)
+
+val is_connected : t -> int -> int -> bool
 (** [is_connected g v w] returns true if [v] and [w] are connected in [g].
     O(n)  *)
-val is_connected : t -> int -> int -> bool
-(** [iter_neighbors f g v] calls [f w] for each neighbor [w] of [v] in [g].  *)
+
 val iter_neighbors : (int -> unit) -> t -> int -> unit
+(** [iter_neighbors f g v] calls [f w] for each neighbor [w] of [v] in [g].  *)
+
+val neighbors_array : t -> int -> int array
 (** [neighbors_array g v] returns an array with the neighbors of [v]
     in [g].  *)
-val neighbors_array : t -> int -> int array
-(** [iter_edges f g] calls [f u v] for each edge [(u, v)] in [g].  *)
+
 val iter_edges : (int -> int -> unit) -> t -> unit
+(** [iter_edges f g] calls [f u v] for each edge [(u, v)] in [g].  *)
+
+val delete_vertex : t -> int -> unit
 (** [delete_vertex g v] removes all edges involving [v] in [g]. [size] is
     unchanged.  *)
-val delete_vertex : t -> int -> unit
+
+val subgraph : t -> BitVec.t -> t
 (** [subgraph g s] returns a copy of [g] where all edges where not both end
     points are in [s]  are removed.  *)
-val subgraph : t -> BitVec.t -> t
