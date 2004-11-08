931fd72634f52e8f09995a6ef52fc783f8b8e336
Falk Hueffner <hueffner@minet.uni-jena.de>
2004-11-08 11:22:58 +0000

    Optionally find edge bipartizations.

--- occ-lp.py
+++ occ-lp.py
@@ -6,20 +6,25 @@ def usage(fd):
     print >> fd, "Usage: occ-lp [-c]"
     print >> fd, "Calculate minimum odd cycle cover by integer linear programming."
     print >> fd, "  -c   Print only the size of the odd cycle cover"
+    print >> fd, "  -s   Print only statistics"
+    print >> fd, "  -e   Cover by edges (default: vertices)"
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hcs", ["--help"])
+        opts, args = getopt.getopt(sys.argv[1:], "hcse", ["--help"])
     except getopt.GetoptError:
         usage(sys.stderr)
         sys.exit(2)
 
     count_only = 0
     stats = 0
+    edge_occ = 0
     for o, a in opts:
         if o in ("-h", "--help"):
             usage(sys.stdout)
             sys.exit(0)
+        if o == "-e":
+            edge_occ = 1
         if o == "-c":
             count_only = 1
         if o == "-s":
@@ -62,6 +67,11 @@ def main():
     n = len(vertex_numbers)
     m = len(edges)
 
+    if edge_occ:
+        occ_range = "m"
+    else:
+        occ_range = "n"
+
     #glpsol_in, glpsol_out = os.popen2("glpsol --math /dev/stdin --display /dev/null --output /dev/stdout")
     glpsol = popen2.Popen3("glpsol --math /dev/stdin --display /dev/null --output /dev/stdout")
 
@@ -69,20 +79,37 @@ def main():
     glpsol_out = glpsol.fromchild
 
     print >> glpsol_in, \
-"""param n > 0 integer;
+"""
+param n > 0 integer;
 param m > 0 integer;
 
 param v1{0..m-1}, >= 0;
 param v2{0..m-1}, >= 0;
-
-var occ{0..n-1} binary;
+"""
+    if edge_occ:
+        print >> glpsol_in, \
+"""
+var occ{0..%s-1} binary;
 var p{0..n-1} binary;
 
-minimize obj: sum{i in 0..n-1} occ[i];
+minimize obj: sum{i in 0..%s-1} occ[i];
 
+""" % (occ_range, occ_range)
+
+    if edge_occ:
+        print >> glpsol_in, \
+"""
+s.t.	neq1{i in 0..m-1}: p[v1[i]] + p[v2[i]] + occ[i] >= 1;
+	neq2{i in 0..m-1}: p[v1[i]] + p[v2[i]] - occ[i] <= 1;
+"""
+    else:
+        print >> glpsol_in, \
+"""
 s.t.	neq1{i in 0..m-1}: p[v1[i]] + p[v2[i]] + occ[v1[i]] + occ[v2[i]] >= 1;
 	neq2{i in 0..m-1}: p[v1[i]] + p[v2[i]] - occ[v1[i]] - occ[v2[i]] <= 1;
-
+"""
+    print >> glpsol_in, \
+"""
 data;
 
 param n := %d;
@@ -115,7 +142,12 @@ param m := %d;
         if match:
             if int(match.groups()[1]):
                 if not count_only:
-                    print vertex_names[int(match.groups()[0])]
+                    occ_el = int(match.groups()[0])
+                    if edge_occ:
+                        edge = edges[occ_el]
+                        print vertex_names[edge[0]], vertex_names[edge[1]]
+                    else:
+                        print vertex_names[occ_el]
                 occ_size += 1
     if stats:
         user, system, child_user, child_system, wall = os.times()
